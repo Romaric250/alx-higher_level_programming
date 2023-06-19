4188fa2e38d58657d5694b46366ce662c790682e
Romaric250 <lonfonyuyromeo@gmail.com>
2023-06-19 11:27:05 -0700

    task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+def new_in_list(my_list, idx, element):
+    if (idx < 0):
+        return (my_list)
+    elif (idx > len(my_list)):
+        return (my_list)
+    else:
+        new_list = my_list[:]
+        new_list[idx] = element
+        return (new_list)
