6e7af68a7d55e8c3b218453f9250243f0d967b8a
Romaric250 <lonfonyuyromeo@gmail.com>
2023-06-26 03:57:40 -0700

    task 2.4

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,9 @@
 #!/usr/python3
 def common_elements(set_1, set_2):
-    new_set = set()
+    common_set = set()
+
     for elem in set_1:
+
         if elem in set_2:
-            new_set.add(elem)
-    return (new_set)
+            common_set.add(elem)
+    return (common_set)
