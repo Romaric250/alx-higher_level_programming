5d88164958474590a91679b4fc5b561733595398
Romaric250 <lonfonyuyromeo@gmail.com>
2023-06-26 05:44:01 -0700

    task a

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
-    return (a_dictionary)
+    return ', '.join([f"{k}: {v}" for k, v in a_dictionary.items()])
