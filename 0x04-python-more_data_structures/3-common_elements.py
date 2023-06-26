707e98c9f20feb50087e6af2f6578cd4c5511a02
Romaric250 <lonfonyuyromeo@gmail.com>
2023-06-26 03:44:18 -0700

    task 3

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/python3
+def common_elements(set_1, set_2_):
+    new_st = set()
+    for elem in set_1:
+        if elem in set_2:
+            new_setr.append(elem)
+        return (new_set)
