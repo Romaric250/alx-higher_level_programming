a1db7da0468f5f6a9c96d84134aaeebbe50930e3
Romaric250 <lonfonyuyromeo@gmail.com>
2023-06-27 02:25:42 -0700

    task 4

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,13 @@
 #!/bin/python3
+
 def best_score(a_dictionary):
-    max_score = float('-inf')
-    best_student = None
-    for student, score in a_dictionary.items():
-        if score > max_score:
-            max_score = score
-            best_student = student
-    return (best_student)
+
+  best_score = None
+  best_key = None
+  for key, value in a_dictionary.items():
+    if best_score is None or value > best_score:
+      best_score = value
+      best_key = key
+
+  return best_key
+
