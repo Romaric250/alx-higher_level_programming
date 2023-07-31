eb10eb182f71f5305fc6b71901719d83b2f9a8a5
Romaric250 <lonfonyuyromeo@gmail.com>
2023-07-31 04:39:52 -1100

    more taks

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,50 +1,54 @@
-#!/usr/bin/python3
-"""Reads from standard input
-"""
+#!/usr/bin/env python3
+
+import sys
+from collections import defaultdict
 
 
-def print_stats(size, status_codes):
-    """Print the accumulated metrics
+def compute_metrics():
+    """Reads standard input line by line and computes metrics.
+
+    Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+    Each 10 lines and after a keyboard interruption (CTRL + C), prints those statistics since the beginning:
+        Total file size: File size: <total size>
+        where is the sum of all previous (see input format above)
+        Number of lines by status code:
+            possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+            if a status code doesn’t appear, don’t print anything for this status code
+            format: <status code>: <number>
+            status codes should be printed in ascending order
     """
-    print("File size: {}".format(size))
-    for key in sorted(status_codes):
-        print("{}: {}".format(key, status_codes[key]))
+
+    # Initialize counters and dictionaries
+    total_file_size = 0
+    status_codes = defaultdict(int)
+
+    try:
+        for i, line in enumerate(sys.stdin):
+            # Split the line into its components
+            components = line.split()
+
+            # Extract the file size and status code
+            file_size = int(components[-1])
+            status_code = int(components[-2])
+
+            # Update the counters and dictionaries
+            total_file_size += file_size
+            status_codes[status_code] += 1
+
+            # Print the metrics every 10 lines
+            if i > 0 and (i + 1) % 10 == 0:
+                print("File size: {}".format(total_file_size))
+                for code in sorted(status_codes.keys()):
+                    print("{}: {}".format(code, status_codes[code]))
+
+    except KeyboardInterrupt:
+        pass
+
+    # Print the final metrics
+    print("File size: {}".format(total_file_size))
+    for code in sorted(status_codes.keys()):
+        print("{}: {}".format(code, status_codes[code]))
 
 
 if __name__ == "__main__":
-    import sys
-
-    size = 0
-    status_codes = {}
-    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    count = 0
-
-    try:
-        for line in sys.stdin:
-            if count == 10:
-                print_stats(size, status_codes)
-                count = 1
-            else:
-                count += 1
-
-            line = line.split()
-
-            try:
-                size += int(line[-1])
-            except (IndexError, ValueError):
-                pass
-
-            try:
-                if line[-2] in valid_codes:
-                    if status_codes.get(line[-2], -1) == -1:
-                        status_codes[line[-2]] = 1
-                    else:
-                        status_codes[line[-2]] += 1
-            except IndexError:
-                pass
-
-        print_stats(size, status_codes)
-
-    except KeyboardInterrupt:
-        print_stats(size, status_codes)
-        raise
+    compute_metrics()
