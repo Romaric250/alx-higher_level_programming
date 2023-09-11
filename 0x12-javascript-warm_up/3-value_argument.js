1bbfb64c22dd0c0feda2b96f71ca5a87030cae7b
Romaric250 <lonfonyuyromeo@gmail.com>
2023-09-11 01:19:28 -1100

    some task done

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,3 +1,9 @@
-#!/usr/bin/node
-import { argv } from 'node:process';
-console.log(process.argv[3]);
+#!/usr/bin/env node
+import { argv } from 'node:process'
+const arg = process.argv[2];
+
+if (arg !== undefined) {
+    console.log(arg);
+} else {
+    console.log("No argument");
+}
