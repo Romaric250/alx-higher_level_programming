bf2f22edb27ddc5525bc01290e2916447dac7e73
Romaric250 <lonfonyuyromeo@gmail.com>
2023-09-12 08:47:24 -0700

    some task updated

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,11 +2,9 @@
 import { argv } from 'node:process';
 
 if (argv.length === 2) {
-	console.log('No argument');
-}
-else if (argv.length === 3) {
-	console.log('Argument found');
-}
-else {
-	console.log('Arguments found');
+  console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
